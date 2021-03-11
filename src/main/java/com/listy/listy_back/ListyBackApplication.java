41b6a587429c95c96679e10b7a30ed0c34cafb35
Lillianpparker <lillianpparker@outlook.com>
2021-03-11 15:24:57 -0600

    changed to Java, added dependencies

---
+++ src/main/java/com/listy/listy_back/ListyBackApplication.java
@@ -0,0 +1,13 @@
+package com.listy.listy_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listy/listy_back/ListyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listy.listy_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
